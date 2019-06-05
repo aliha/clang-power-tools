fd68a31f883aa593189a3c687477a7e43569438e
Enache Ionut <ionut.enache@caphyon.com>
2019-06-05 17:56:44 +0300

    add custom error window for unexpected errors

---
+++ ClangPowerTools/ClangPowerTools/ViewModels/UnexpectedErrorViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ClangPowerTools.ViewModels
+{
+  public class UnexpectedErrorViewModel
+  {
+    private string mMessage;
+
+    public string Message
+    {
+      get { return mMessage; }
+      set { mMessage = value; }
+    }
+
+    public UnexpectedErrorViewModel(string message) => mMessage = message;
+
+  }
+}
---
+++ ClangPowerTools/ClangPowerTools/Views/UnexpectedErrorView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ClangPowerTools.ViewModels;
+using System.Windows;
+
+namespace ClangPowerTools.Views
+{
+  /// <summary>
+  /// Interaction logic for UnexpectedErrorView.xaml
+  /// </summary>
+  public partial class UnexpectedErrorView : Window
+  {
+    public UnexpectedErrorView(string message)
+    {
+      InitializeComponent();
+      var errorViewModel = new UnexpectedErrorViewModel(message);
+      DataContext = errorViewModel;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
