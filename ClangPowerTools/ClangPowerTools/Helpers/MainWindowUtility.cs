481ad328784fd03b41ff4bb541c5c97358ab450e
Enache Ionut <ionut.enache@caphyon.com>
2019-06-06 12:04:34 +0300

    get the main window pointer

---
+++ ClangPowerTools/ClangPowerTools/Helpers/MainWindowUtility.cs
@@ -0,0 +1,15 @@
+﻿using ClangPowerTools.Services;
+using EnvDTE;
+using System;
+
+namespace ClangPowerTools.Helpers
+{
+  public class MainWindowUtility
+  {
+    public static IntPtr GetPointer()
+    {
+      var dte = (DTE)VsServiceProvider.GetService(typeof(DTE));
+      return dte.MainWindow != null ? (IntPtr)dte.MainWindow.HWnd : IntPtr.Zero;
+    }
+  }
+}
